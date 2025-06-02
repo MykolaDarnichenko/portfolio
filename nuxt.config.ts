5de262d166676baa100ab9079b19bd4cc4fd826e
Mykola Darnichenko <programmboy@gmail.com>
2025-06-02 20:05:58 +0000

    add changes for gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: false,
+  app: {
+    baseURL: '/portfolio/',
+  },
   components: true, // включен автоимпорт
   css: ['~/assets/css/fonts.css', '~/assets/css/main.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
